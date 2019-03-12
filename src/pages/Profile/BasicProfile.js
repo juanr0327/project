808e5789b4064c306035913ab86261fa35c39722
tianzhich <tianzhi@kuaishou.com>
2019-03-12 11:30:01 +0800

    fix: eslint

--- src/pages/Profile/BasicProfile.js
+++ src/pages/Profile/BasicProfile.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { connect } from 'dva';
-import { Card, Badge, Table, Divider } from 'antd';
+import { Card, Badge, Table, Divider, Form, Modal, Button } from 'antd';
 import DescriptionList from '@/components/DescriptionList';
 import PageHeaderWrapper from '@/components/PageHeaderWrapper';
 import styles from './BasicProfile.less';
@@ -39,8 +39,61 @@ const progressColumns = [
     dataIndex: 'cost',
     key: 'cost',
   },
+  {
+    title: '操作',
+    render: () => (
+      <Fragment>
+        <App />
+      </Fragment>
+    ),
+  },
 ];
 
+class App extends Component {
+  state = { visible: false }
+
+  showModal = () => {
+    this.setState({
+      visible: true,
+    });
+  }
+
+  handleOk = (e) => {
+    console.log(e);
+    this.setState({
+      visible: false,
+    });
+  }
+
+  handleCancel = (e) => {
+    console.log(e);
+    this.setState({
+      visible: false,
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <Button type="primary" onClick={this.showModal}>
+          查看图片
+        </Button>
+        <Modal
+          title="图片详情"
+          visible={this.state.visible}
+          onOk={this.handleOk}
+          onCancel={this.handleCancel}
+        >
+          <p>Some contents...</p>
+          <p>Some contents...</p>
+          <p>Some contents...</p>
+        </Modal>
+      </div>
+    );
+  }
+}
+
+/* eslint react/no-multi-comp:0 */
 @connect(({ profile, loading }) => ({
   profile,
   loading: loading.effects['profile/fetchBasic'],
@@ -56,122 +109,35 @@ class BasicProfile extends Component {
     });
   }
 
+
+
   render() {
+
     const { profile = {}, loading } = this.props;
-    const { basicGoods = [], basicProgress = [], userInfo = {}, application = {} } = profile;
-    let goodsData = [];
-    if (basicGoods.length) {
-      let num = 0;
-      let amount = 0;
-      basicGoods.forEach(item => {
-        num += Number(item.num);
-        amount += Number(item.amount);
-      });
-      goodsData = basicGoods.concat({
-        id: '总计',
-        num,
-        amount,
-      });
-    }
-    const renderContent = (value, row, index) => {
-      const obj = {
-        children: value,
-        props: {},
-      };
-      if (index === basicGoods.length) {
-        obj.props.colSpan = 0;
-      }
-      return obj;
-    };
-    const goodsColumns = [
-      {
-        title: '商品编号',
-        dataIndex: 'id',
-        key: 'id',
-        render: (text, row, index) => {
-          if (index < basicGoods.length) {
-            return <a href="">{text}</a>;
-          }
-          return {
-            children: <span style={{ fontWeight: 600 }}>总计</span>,
-            props: {
-              colSpan: 4,
-            },
-          };
-        },
-      },
-      {
-        title: '商品名称',
-        dataIndex: 'name',
-        key: 'name',
-        render: renderContent,
-      },
-      {
-        title: '商品条码',
-        dataIndex: 'barcode',
-        key: 'barcode',
-        render: renderContent,
-      },
-      {
-        title: '单价',
-        dataIndex: 'price',
-        key: 'price',
-        align: 'right',
-        render: renderContent,
-      },
-      {
-        title: '数量（件）',
-        dataIndex: 'num',
-        key: 'num',
-        align: 'right',
-        render: (text, row, index) => {
-          if (index < basicGoods.length) {
-            return text;
-          }
-          return <span style={{ fontWeight: 600 }}>{text}</span>;
-        },
-      },
-      {
-        title: '金额',
-        dataIndex: 'amount',
-        key: 'amount',
-        align: 'right',
-        render: (text, row, index) => {
-          if (index < basicGoods.length) {
-            return text;
-          }
-          return <span style={{ fontWeight: 600 }}>{text}</span>;
-        },
-      },
-    ];
+    const { basicProgress = [], userInfo = {}, application = {} } = profile;
+
+
     return (
-      <PageHeaderWrapper title="基础详情页" loading={loading}>
+
+      <PageHeaderWrapper title="转账详情页" loading={loading}>
         <Card bordered={false}>
-          <DescriptionList size="large" title="退款申请" style={{ marginBottom: 32 }}>
-            <Description term="取货单号">{application.id}</Description>
-            <Description term="状态">{application.status}</Description>
-            <Description term="销售单号">{application.orderNo}</Description>
-            <Description term="子订单">{application.childOrderNo}</Description>
+          <DescriptionList size="large" title="基本信息" style={{ marginBottom: 32 }}>
+            <Description term="转入银行">{application.id}</Description>
+            <Description term="转入账户">{application.status}</Description>
+            <Description term="转账时间">{application.orderNo}</Description>
+            <Description term="转账金额">{application.childOrderNo}</Description>
           </DescriptionList>
           <Divider style={{ marginBottom: 32 }} />
-          <DescriptionList size="large" title="用户信息" style={{ marginBottom: 32 }}>
-            <Description term="用户姓名">{userInfo.name}</Description>
-            <Description term="联系电话">{userInfo.tel}</Description>
-            <Description term="常用快递">{userInfo.delivery}</Description>
-            <Description term="取货地址">{userInfo.addr}</Description>
+          <DescriptionList size="large" title="详细信息" style={{ marginBottom: 32 }}>
+            <Description term="转出银行">{userInfo.name}</Description>
+            <Description term="转出账户">{userInfo.tel}</Description>
+            <Description term="操作员姓名">{userInfo.delivery}</Description>
+            <Description term="完成状态">{userInfo.addr}</Description>
             <Description term="备注">{userInfo.remark}</Description>
           </DescriptionList>
           <Divider style={{ marginBottom: 32 }} />
-          <div className={styles.title}>退货商品</div>
-          <Table
-            style={{ marginBottom: 24 }}
-            pagination={false}
-            loading={loading}
-            dataSource={goodsData}
-            columns={goodsColumns}
-            rowKey="id"
-          />
-          <div className={styles.title}>退货进度</div>
+
+          <div className={styles.title}>进度</div>
           <Table
             style={{ marginBottom: 16 }}
             pagination={false}
@@ -179,6 +145,7 @@ class BasicProfile extends Component {
             dataSource={basicProgress}
             columns={progressColumns}
           />
+
         </Card>
       </PageHeaderWrapper>
     );
